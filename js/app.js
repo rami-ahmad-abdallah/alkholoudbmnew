d14832f8c897155008dc4d3c72ca7930fe540b26
rami <rami.ahmad.abdullah@gmail.com>
2026-05-23 15:44:08 +0600

    alkholoudbm.com 7

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let nav = document.querySelector("nav");
 let burger = document.querySelector(".burger");
-let menuLinks = document.querySelectorAll(".menu-link");
+let menu = document.querySelector(".menu");
 let headerSlides = document.querySelectorAll(".header-slide");
 let currentSlide = 0;
 
@@ -10,30 +10,11 @@ nav.addEventListener("click", (e) => {
     e.target.classList.contains("burger")
   ) {
     burger.classList.toggle("active");
-    nav.classList.toggle("active");
-
-    if (nav.classList.contains("active")) {
-      addTransitionDelay(menuLinks);
-    } else {
-      addTransitionDelay([...menuLinks].reverse());
-    }
+    menu.classList.toggle("active");
   }
 });
 
-// ADDS A TRANSITION DELAY TO A GIVEN LIST OF HTML NODES
-function addTransitionDelay(arr, delay = 0) {
-  arr.forEach((item) => {
-    delay += 0.05;
-    item.style.transitionDelay = `${delay}s`;
-  });
-}
 
-// ADDS A TRANSITION DELAY OF 0 TO A GIVEN LIST OF HTML NODES
-function removeTransitionDelay(arr) {
-  arr.forEach((item) => {
-    item.style.transitionDelay = "0s";
-  });
-}
 
 function changeSlide() {
   if (currentSlide === headerSlides.length - 1) {
